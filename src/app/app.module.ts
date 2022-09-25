34fdd1af43297e48a1030313e0a8660711395647
Devrim Furkan GUNER <70522562+devrimfurkan@users.noreply.github.com>
2022-09-25 16:31:46 +0300

    Create Custom Pipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SummaryPipe } from './summary.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SummaryPipe
   ],
   imports: [
     BrowserModule
